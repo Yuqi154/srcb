0c72bfa8cea9deecb49ba4bd5d2b8d171843c2ef
Hei PIoa <heipiao233@outlook.com>
2024-02-21 17:48:15 +0800

    Remove unused imports

--- src/main/java/net/heipiao/mixin/CommandBlockMinecartEntityMixin.java
+++ src/main/java/net/heipiao/mixin/CommandBlockMinecartEntityMixin.java
@@ -1,9 +1,7 @@
 package net.heipiao.mixin;
 
-import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.CommandBlockMinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
